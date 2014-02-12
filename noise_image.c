4bc54aa5df3691b018cba213e7fa002e4394c353
ponos <ponos@koszmail.pl>
2014-02-11 23:46:26 -0200

    Apply a random displacement for each octave

--- noise_image.c
+++ noise_image.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #define WIDTH		1024
 #define HEIGHT		1024
@@ -8,10 +9,10 @@
 
 double	noise[WIDTH][HEIGHT];
 
-void             rough_noise();
-double           smooth_noise(double x, double y);
-double           turbulence(double x, double y, double size);
-unsigned int     random_in_range (unsigned int min, unsigned int max);
+void      rough_noise();
+double    smooth_noise(double x, double y);
+double    turbulence(double x, double y, double size);
+double    random_in_range (double min, double max);
 
 static struct color {
     unsigned char r, g, b;
@@ -33,9 +34,9 @@ int main(int argc, char *argv[])
     for(x = 0; x < WIDTH; ++x)
         for(y = 0; y < HEIGHT; ++y) {
             /* random_in_range is too noisy, so we use pre-defined numbers for now. */
-            fprintf(fp, "%c", itr->r  = turbulence(x, y, 96));
-            fprintf(fp, "%c", itr->g  = turbulence(x, y, 32));
-            fprintf(fp, "%c", itr->b  = turbulence(x, y, 64));
+            fprintf(fp, "%c", itr->r  = turbulence(x, y, 32));
+            fprintf(fp, "%c", itr->g  = turbulence(x, y, 64));
+            fprintf(fp, "%c", itr->b  = turbulence(x, y, 96));
             itr++;
         }
 
@@ -80,18 +81,30 @@ double smooth_noise(double x, double y)
 double turbulence(double x, double y, double size)
 {
     double value = 0.0, initial_size = size;
+    int int_size = (int)floor(size);
+    int offset_x[int_size], offset_y[int_size];
+
+    int a, b;
+
+    for (a = 0; a < int_size; ++a) {
+        offset_x[a] = (rand() % RAND_MAX) / (double)RAND_MAX;
+    }
+    for (b = 0; b < int_size; ++b) {
+        offset_y[b] = (rand() % RAND_MAX) / (double)RAND_MAX;
+    }
 
     while(size >= 1) {
-        value += smooth_noise(x / size, y / size) * size;
+        value += smooth_noise(x / size + offset_x[int_size], y / size + offset_y[int_size]) * size;
         size /= 2.0;
+        int_size /= 2.0;
     }
 
     return(128.0 * value / initial_size);
 }
 
-unsigned int random_in_range (unsigned int min, unsigned int max)
+double random_in_range (double min, double max)
 {
     srand((unsigned)time(NULL));
-    unsigned int result = min + rand() / (RAND_MAX / (max - min + 1) + 1);
+    double result = min + rand() / (RAND_MAX / (max - min + 1.0) + 1.0);
     return result;
 }
